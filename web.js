77b5ef0c898bc25f902c0e9e3a27bc422b66d475
Juan González <juan.gonzalez@kasna.com.au>
2022-09-09 11:55:59 +0800

    remove logs

--- web.js
+++ web.js
@@ -9,7 +9,6 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body);
   const moves = ['F','F','F', 'T', 'L', 'R'];
   res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
